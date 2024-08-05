542af8c1f94b3dfc9cb2562c3dcaf68a250a21ee
sadiq_from_ubuntu_server <sadiqonemail@gmail.com>
2024-08-05 22:25:58 +0000

    testing changes

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express()
 const PORT = process.env.PORT || 8080
 
-app.get('/', (req, res) => res.send('api is running !!!'))
+app.get('/', (req, res) => res.send('api is running !!! !!!'))
 
 app.listen(PORT, () => console.log(`server is running http://localhost:${PORT}`))
